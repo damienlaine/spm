6f98b39e75f0bfc9f2e3ad3cc5dd2a3e00f52e03
damienlaine <damien.laine@gmail.com>
2024-01-07 17:22:27 +0100

    update

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1704622245';
+const cacheName = 'chirpy-1704644373';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
